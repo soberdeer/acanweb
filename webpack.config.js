548e54991b22b26406b395e82ea4aa693020c79e
soberdeer <rarerial@mail.ru>
2019-11-27 17:08:28 +0300

    -

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,40 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.jsx',
-  output: {
-    path: path.join(__dirname, '/dist'),
-    filename: 'index_bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
+    entry: './src/index.jsx',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'index_bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }],
+            },
+        ],
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            favicon: './src/favicon.ico'
+        }),
     ],
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      favicon: './src/favicon.ico'
-    }),
-  ],
 };
