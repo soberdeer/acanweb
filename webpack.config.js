68185e7d7568d27dc76ad4595a1eed700894d084
soberdeer <soberdeer@users.noreply.github.com>
2019-11-27 16:41:13 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
-      favicon: "./favicon.ico"
+      favicon: './src/favicon.ico'
     }),
   ],
 };
